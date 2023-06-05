2dbf7d3c66ec80ffe5d482ae031851b9b89d16dd
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-06-05 11:14:41 +0000

    Created my first stetful component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
       <FunctionalGreetingWithProps greeting="Nice to meet you" age="24" name="Yoel" />
+      <StatefulGreeting greeting="I'm a stateful class component"/>
     </div>
   );
 }
---
+++ my-app/src/components/StatefulGreeting.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class StatefulGreeting extends React.Component {
+    render(){
+        return <h1>Hello {this.props.greeting}</h1>
+    }
+
+}
+
+export default StatefulGreeting
