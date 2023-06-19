283041465267831b54d992c41b2a766db4df303e
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-06-19 17:26:01 +0000

    Created cheetsheet of es7 snippets

---
+++ my-app/src/components/Es7Snippets.js
@@ -0,0 +1,49 @@
+// rfce
+// import React from 'react'
+
+// function Es7Snippets() {
+//   return (
+//     <div>Es7Snippets</div>
+//   )
+// }
+
+// export default Es7Snippets
+
+// rafce
+
+// import React from 'react'
+
+// const Es7Snippets = () => {
+//   return (
+//     <div>Es7Snippets</div>
+//   )
+// }
+
+// export default Es7Snippets
+
+// rce
+
+// import React, { Component } from 'react'
+
+// export class Es7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>Es7Snippets</div>
+//     )
+//   }
+// }
+
+// export default Es7Snippets
+
+// rconst
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// clo
+// console.log('first', first)
