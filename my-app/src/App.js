556f88196d1b2c2386bd064d83f1cac223eb74f8
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-06-20 07:19:41 +0000

    Created NestingComponents component parent and children components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,12 +6,12 @@ import EventsClassComponent from './components/EventsClassComponent';
 import EventsFunctional from './components/EventsFunctional';
 import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+import NestingComponents from './components/NestingComponents';
 
 function App() {
   return (
     <div className="App">
-      <ConditionalRenderingClass/>
-      <ConditionalRenderingFunctional connected={false}/>
+      <NestingComponents/>
     </div>
   );
 }
---
+++ my-app/src/components/NestingComponents.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import UserData from './UserData'
+import UserMessage from './UserMessage'
+
+export class NestingComponents extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        isLoaded: false,
+        isLoggedIn: true,
+      }
+    }
+  render() {
+    return (
+      <div>
+        <UserData isLoaded={this.state.isLoaded} />
+        <UserMessage isLoggedIn={this.state.isLoggedIn} />
+      </div>
+    )
+  }
+}
+
+export default NestingComponents
