96b42e078c880fd4652cb57eeaf68a6a4551bdde
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-06-20 11:42:21 +0000

    Creates Methodsasprops parent and child components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,11 +7,12 @@ import EventsFunctional from './components/EventsFunctional';
 import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 import NestingComponents from './components/NestingComponents';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 function App() {
   return (
     <div className="App">
-      <NestingComponents/>
+      <MethodsAsPropsParent/>
     </div>
   );
 }
