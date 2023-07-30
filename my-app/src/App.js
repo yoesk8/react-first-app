5008116dd6da7632855e432a993a66ba5a075dbc
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-07-30 12:43:42 +0000

    Created usestate with objects component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,12 +17,13 @@ import UncontrolledForm from './components/UncontrolledForm';
 import SearchBar from './components/SearchBar';
 import ControlledFormHooks from './components/ControlledFormHooks';
 import UseStateWithArrays from './components/UseStateWithArrays';
+import UseStateWithObjects from './components/UseStateWithObjects';
 
 
 function App() {
   return (
     <div className="App">
-      <UseStateWithArrays/>
+      <UseStateWithObjects/>
     </div>
   );
 }
