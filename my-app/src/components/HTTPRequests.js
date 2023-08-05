98c782eb9dc3a696e5d77a6d6b960c7a9bcdc36c
yoesk8 <yoelgutierrez2021@yahoo.com>
2023-08-05 21:14:47 +0000

    Added formatting to json api response, added catch error method and display error to user

--- my-app/src/components/HTTPRequests.js
+++ my-app/src/components/HTTPRequests.js
@@ -3,29 +3,53 @@ import axios from 'axios';
 
 export class HTTPRequests extends Component {
     constructor(props) {
-      super(props)
+        super(props)
     
-      this.state = {
-         posts: []
-      }
+        this.state = {
+             posts: [],
+             error: null
+        }
     }
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/posts/1123123123')
         .then(response => {
             console.log(response);
             this.setState({
-                posts: response.data
+                posts: Array.isArray(response.data)
+                ? response.data
+                : [response.data]
+            })
+        })
+        .catch(error => {
+            this.setState({
+                error: error.message
             })
         })
     }
-  render() {
-    return (
-      <div>
-        <h2>Posts:</h2>
-        {JSON.stringify(this.state.posts)}
-      </div>
-    )
-  }
+    render() {
+        const posts = this.state.posts;
+        return (
+            <div>
+                <h2>Posts:</h2>
+                {
+                    posts.length ? (
+                        posts.map(post => (
+                            <div key={post.id}>
+                                <h2>{post.id}. {post.title}</h2>
+                                <h4>By User ID {post.userId}</h4>
+                                <p>{post.body}</p>
+                                <hr />
+                            </div>
+                        ))
+                    ) : (
+                        this.state.error
+                        ? <p>{this.state.error}</p>
+                        : <h4>Loading posts ...</h4>
+                    )
+                }
+            </div>
+        )
+    }
 }
 
 export default HTTPRequests
